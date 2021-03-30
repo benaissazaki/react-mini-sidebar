86141ad92afba368fca4c0e3d01393e4d771f1bf
lilxak <zakben45@gmail.com>
2021-03-30 15:22:06 +0100

    extendOnHover doesn't apply to mobile now

--- src/MiniSidebar.jsx
+++ src/MiniSidebar.jsx
@@ -1,17 +1,20 @@
-import React, { Fragment } from "react"
-import { useState } from "react";
-import styles from "./MiniSidebar.module.css";
+import React, { Fragment } from 'react'
+import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import styles from './MiniSidebar.module.css'
+
 
 export const MiniSidebar = ({ children, collapseOnMd = false, withOverlay = true, extendOnHover = false }) => {
     const [collapsed, setCollapsed] = useState(true);
+    const isMobile = useMediaQuery({query: '(max-width: 768px)'})
 
     return (
         <>
             <div className={`${styles.overlay} ${!collapsed && withOverlay ? styles.active : ''} ${!collapsed && collapseOnMd && withOverlay ? styles.active_md : ""}`}></div>
-            <aside onMouseEnter={() => extendOnHover && setCollapsed(false)} onMouseLeave={() => extendOnHover && setCollapsed(true)} className={`${styles.sidebar} ${collapsed ? "" : styles.expanded} ${collapseOnMd && styles.sidebar_collapsable_md} ${!collapsed && collapseOnMd ? styles.expanded_md : ""}`}>
+            <aside onMouseEnter={() => (extendOnHover && !isMobile) && setCollapsed(false)} onMouseLeave={() => (extendOnHover && !isMobile) && setCollapsed(true)} className={`${styles.sidebar} ${collapsed ? "" : styles.expanded} ${collapseOnMd && styles.sidebar_collapsable_md} ${!collapsed && collapseOnMd ? styles.expanded_md : ""}`}>
                 {children}
 
-                {!extendOnHover &&
+                {(!extendOnHover || isMobile) &&
                     <button
                         className={`${styles.sidebar_collapse_btn} ${collapseOnMd ? styles.sidebar_collapse_btn_md : ""} ${collapsed ? styles.sidebar_collapse_btn_collapsed : styles.sidebar_collapse_btn_expanded}`}
                         onClick={() => setCollapsed(!collapsed)}>
