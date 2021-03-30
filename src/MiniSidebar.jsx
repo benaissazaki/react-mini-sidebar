1c9f7ce22fb8058e8dba7e776c54653416d01143
lilxak <zakben45@gmail.com>
2021-03-30 15:06:47 +0100

    Add new props

--- src/MiniSidebar.jsx
+++ src/MiniSidebar.jsx
@@ -2,19 +2,22 @@ import React, { Fragment } from "react"
 import { useState } from "react";
 import styles from "./MiniSidebar.module.css";
 
-export const MiniSidebar = ({ children, collapseOnMd = false }) => {
+export const MiniSidebar = ({ children, collapseOnMd = false, withOverlay = true, extendOnHover = false }) => {
     const [collapsed, setCollapsed] = useState(true);
 
     return (
         <>
-            <div className={`${styles.overlay} ${collapsed ? "" : styles.active} ${!collapsed && collapseOnMd ? styles.active_md : ""}`}></div>
-            <aside className={`${styles.sidebar} ${collapsed ? "" : styles.expanded} ${collapseOnMd && styles.sidebar_collapsable_md} ${!collapsed && collapseOnMd ? styles.expanded_md : "" }`}>
+            <div className={`${styles.overlay} ${!collapsed && withOverlay ? styles.active : ''} ${!collapsed && collapseOnMd && withOverlay ? styles.active_md : ""}`}></div>
+            <aside onMouseEnter={() => extendOnHover && setCollapsed(false)} onMouseLeave={() => extendOnHover && setCollapsed(true)} className={`${styles.sidebar} ${collapsed ? "" : styles.expanded} ${collapseOnMd && styles.sidebar_collapsable_md} ${!collapsed && collapseOnMd ? styles.expanded_md : ""}`}>
                 {children}
-                <button
-                    className={`${styles.sidebar_collapse_btn} ${collapseOnMd ? styles.sidebar_collapse_btn_md : ""} ${collapsed ? styles.sidebar_collapse_btn_collapsed : styles.sidebar_collapse_btn_expanded}`}
-                    onClick={() => setCollapsed(!collapsed)}>
-                    →
-                </button>
+
+                {!extendOnHover &&
+                    <button
+                        className={`${styles.sidebar_collapse_btn} ${collapseOnMd ? styles.sidebar_collapse_btn_md : ""} ${collapsed ? styles.sidebar_collapse_btn_collapsed : styles.sidebar_collapse_btn_expanded}`}
+                        onClick={() => setCollapsed(!collapsed)}>
+                        →
+                    </button>
+                }
             </aside>
         </>
     )
