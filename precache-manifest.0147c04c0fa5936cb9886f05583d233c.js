7b5d3f4a9122bf1afc65f074a137f15ddaaecd5b
lilxak <zakben45@gmail.com>
2021-03-31 15:43:06 +0100

    Updates

---
+++ precache-manifest.0147c04c0fa5936cb9886f05583d233c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e48a2abe784e54a3be4d19c2360223",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42c4f6969cf63300e3ea",
+    "url": "./static/css/main.02c59eac.chunk.css"
+  },
+  {
+    "revision": "c288d83554d596292060",
+    "url": "./static/js/2.86573380.chunk.js"
+  },
+  {
+    "revision": "5bdacc385fa6e96a4d70fd13632c4407",
+    "url": "./static/js/2.86573380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c4f6969cf63300e3ea",
+    "url": "./static/js/main.ac87c60e.chunk.js"
+  },
+  {
+    "revision": "45a4e71cef85139fa7e4",
+    "url": "./static/js/runtime-main.27736fe1.js"
+  }
+]);
