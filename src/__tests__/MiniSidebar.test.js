18619c78ee71cb2579be5f43797e157c8d790052
lilxak <zakben45@gmail.com>
2021-03-31 15:01:15 +0100

    prettify and delete useless test file

--- src/__tests__/MiniSidebar.test.js
+++ src/__tests__/MiniSidebar.test.js
@@ -1,31 +1,31 @@
 import { MiniSidebar } from '../MiniSidebar'
-import { screen, fireEvent, getByText, render } from '@testing-library/react';
-import React from 'react';
+import { screen, fireEvent, render } from '@testing-library/react'
+import React from 'react'
 
 describe('MiniSidebar with collapseOnMd', () => {
-    it('renders correctly', () => {
-        render(<MiniSidebar></MiniSidebar>)
-    })
+  it('renders correctly', () => {
+    expect(render(<MiniSidebar />)).toBeTruthy()
+  })
 
-    it("activates overlay when expanded and desactivates when collapsed", () => {
-        const overlay = render(<MiniSidebar collapseOnMd={true}></MiniSidebar>).container.firstChild
-        expect(overlay.classList.contains('overlay')).toBe(true)                // Make sure it has the 'overlay' class
+  it('activates overlay when expanded and desactivates when collapsed', () => {
+    const overlay = render(<MiniSidebar collapseOnMd />).container.firstChild
+    expect(overlay.classList.contains('overlay')).toBe(true) // Make sure it has the 'overlay' class
 
-        fireEvent.click(screen.getByText('→'))
-        expect(overlay.classList.contains('active_md')).toBe(true)              // Check if the overlay is active after clicking the button
+    fireEvent.click(screen.getByText('→'))
+    expect(overlay.classList.contains('active_md')).toBe(true) // Check if the overlay is active after clicking the button
 
-        fireEvent.click(screen.getByText('→'))
-        expect(overlay.classList.contains('active_md')).toBe(false)             // Check if the overlay is inactive after clicking the button again
-    })
+    fireEvent.click(screen.getByText('→'))
+    expect(overlay.classList.contains('active_md')).toBe(false) // Check if the overlay is inactive after clicking the button again
+  })
 
-    it("expands and collapses when button clicked", () => {
-        const sidebar = render(<MiniSidebar collapseOnMd={true}></MiniSidebar>).container.lastChild
-        expect(sidebar.classList.contains('sidebar_collapsable_md')).toBe(true)                     // Make sure it has the 'sidebar_collapsable_md' class
+  it('expands and collapses when button clicked', () => {
+    const sidebar = render(<MiniSidebar collapseOnMd />).container.lastChild
+    expect(sidebar.classList.contains('sidebar_collapsable_md')).toBe(true) // Make sure it has the 'sidebar_collapsable_md' class
 
-        fireEvent.click(screen.getByText('→'))
-        expect(sidebar.classList.contains('expanded_md')).toBe(true)                                // Check if the sidebar is expanded after clicking the button
+    fireEvent.click(screen.getByText('→'))
+    expect(sidebar.classList.contains('expanded_md')).toBe(true) // Check if the sidebar is expanded after clicking the button
 
-        fireEvent.click(screen.getByText('→'))
-        expect(sidebar.classList.contains('expanded_md')).toBe(false)                               // Check if the sidebar is collapsed after clicking the button again
-    })
+    fireEvent.click(screen.getByText('→'))
+    expect(sidebar.classList.contains('expanded_md')).toBe(false) // Check if the sidebar is collapsed after clicking the button again
+  })
 })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { MiniSidebar } from "./MiniSidebar"
-import { MiniSidebarHeader } from "./MiniSidebarHeader"
-import { MiniSidebarNavigation } from "./MiniSidebarNavigation"
-import { MiniSidebarNavigationitem } from "./MiniSidebarNavigationItem"
-import { MiniSidebarNavigationIcon } from "./MiniSidebarNavigationIcon"
-import { MiniSidebarNavigationLabel } from "./MiniSidebarNavigationLabel";
+import React from 'react'
+import { MiniSidebar } from './MiniSidebar'
+import { MiniSidebarHeader } from './MiniSidebarHeader'
+import { MiniSidebarNavigation } from './MiniSidebarNavigation'
+import { MiniSidebarNavigationitem } from './MiniSidebarNavigationItem'
+import { MiniSidebarNavigationIcon } from './MiniSidebarNavigationIcon'
+import { MiniSidebarNavigationLabel } from './MiniSidebarNavigationLabel'
 
 export {
   MiniSidebar,
