0e0aed94d400e026678527ace51e772b703e9277
lilxak <zakben45@gmail.com>
2021-04-01 13:32:40 +0100

    Create Overlay styled component

--- src/MiniSidebar.jsx
+++ src/MiniSidebar.jsx
@@ -3,14 +3,32 @@ import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import styles from './MiniSidebar.module.css'
 import Proptypes from 'prop-types'
+import styled from 'styled-components'
+
+const Overlay = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${props => props.activeMd ? 'rgba(0, 0, 0, 0.5)' :'rgba(0, 0, 0, 0)'};
+    transition: background-color 1s;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    pointer-events: none;
+    ${props => props.active &&
+        `@media(max-width) {
+            background-color: rgba(0, 0, 0, 0.5);
+        }`
+    }
+`
 
 export const MiniSidebar = ({ children, collapseOnMd = false, withOverlay = true, expandOnHover = false }) => {
     const [collapsed, setCollapsed] = useState(true);
-    const isMobile = useMediaQuery({query: '(max-width: 768px)'})
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
     return (
         <>
-            <div className={`${styles.overlay} ${!collapsed && withOverlay ? styles.active : ''} ${!collapsed && collapseOnMd && withOverlay ? styles.active_md : ""}`}></div>
+            <Overlay active={!collapsed && withOverlay} activeMd={!collapsed && collapseOnMd && withOverlay} />
             <aside onMouseEnter={() => (expandOnHover && !isMobile) && setCollapsed(false)} onMouseLeave={() => (expandOnHover && !isMobile) && setCollapsed(true)} className={`${styles.sidebar} ${collapsed ? "" : styles.expanded} ${collapseOnMd && styles.sidebar_collapsable_md} ${!collapsed && collapseOnMd ? styles.expanded_md : ""}`}>
                 {children}
 
